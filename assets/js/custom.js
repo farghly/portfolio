85e08ebbf80ae9124f534c55cddda1fd26b20828
farghly <eng_farghlyahmed@yahoo.com>
2022-08-03 22:41:45 +0200

    fix loader

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,7 +1,8 @@
 /* loader event hide after loading */
 window.addEventListener('load', () => {
     setTimeout(() => {
-        document.querySelector('.load-container').style.display = 'none'
-    })
-},1000)
+        const loaderContainer = document.querySelector('.load-container');
+        loaderContainer.style.display = 'none';
+    },1000)
+})
 /* end of loader event */
